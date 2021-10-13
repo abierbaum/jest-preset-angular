d68f3396a11303bcd11d70a2658fed39283bb426
Allen Bierbaum <abierbaum@gmail.com>
2021-10-13 12:30:39 -0500

    Add example of bug when compiling.

--- examples/example-app-v12/src/app/app.component.spec.ts
+++ examples/example-app-v12/src/app/app.component.spec.ts
@@ -1,9 +1,25 @@
+import { Component, Input } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
+
+
+@Component({
+   selector: 'test-comp',
+   template: `
+      <div>Testing</div>
+   `,
+})
+export class JestInlineSnapBugComponent {
+   @Input() value1: string = 'val1';
+}
+
+
+
 describe('AppComponent', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -15,6 +31,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+    expect(fixture).toMatchInlineSnapshot();
   });
 
   it(`should have as title 'example-app-v12'`, () => {
